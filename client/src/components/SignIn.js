abaf66a92d416c5b98408da559c1c0eaa481f9b6
Vllaadd <vlladdiimiir@gmail.com>
2021-01-14 01:57:14 -0500

    form created

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,5 +1,13 @@
 import React from "react";
 
 export const SignIn = () => {
-    return <div></div>;
+    return(
+        <div>
+            <label for="emailaddress">Email Address</label>
+            <input type="email" name="emailaddress" />
+            <label for="password">Password</label>
+            <input type="password" name="password" />
+            <input type="submit" value="Sign In" />
+        </div>
+    ) 
 };
