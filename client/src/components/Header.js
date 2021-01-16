9a0fb12c961006c0da802ee4f48b9b1e0bc84017
Vllaadd <vlladdiimiir@gmail.com>
2021-01-16 11:48:51 -0500

    float added

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 import TopGlassCover from "../img/svg/top-glass-cover.svg";
+import { SignIn } from "./SignIn";
 
 export const Header = () => {
     return (
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
 
 import { Header } from "../components/Header";
-import { SignIn } from "../components/SignIn";
+
 
 export default class Dashboard extends Component {
     render() {
         return (
             <>
                 <Header />
-                <SignIn />
             </>
         );
     }
