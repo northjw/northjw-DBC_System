f72ed82d28f79207b580b19315fc4ac18001d14c
Eric Vondunn <ericvondunn40@gmail.com>
2021-01-16 16:19:04 -0500

    unstaged will delete

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-<<<<<<< HEAD
 import TopGlassCover from "./img/svg/top-glass-cover";
 
 export const Header = () => {
@@ -13,19 +12,4 @@ export const Header = () => {
       <div className="underline"></div>
     </div>
   );
-=======
-import TopGlassCover from "../img/svg/top-glass-cover.svg";
-
-export const Header = () => {
-    useLogRender();
-    return (
-        <div className="top-container">
-            <div className="top-glass-cover">
-                <image src={TopGlassCover} />
-            </div>
-            <div className="header"></div>
-            <div className="underline"></div>
-        </div>
-    );
->>>>>>> main
 };
