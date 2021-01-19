c47d56c38d2de4c7e7acbf8fd9f85a7429592abb
Eric Vondunn <ericvondunn40@gmail.com>
2021-01-19 18:13:51 -0500

    upload

--- server/routes/api/card/index.js
+++ server/routes/api/card/index.js
@@ -1,6 +1,27 @@
 const router = require("express").Router();
 const cardController = require("../../../controllers/cardController");
+const require = require("multer");
 
+//define storage for image files
+
+const storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, "./public/uploads/images");
+  },
+
+  //add bck file extension (multer removes it)
+  filename:function (req, file, callback) {
+      callback(null, Date.now() file.originalname)
+  },
+
+  //upload parameters for multer
+  const upload = multer({
+    storage: storage,
+    limits: {
+        fieldSize: 1024 * 1024 * 3,
+  },
+  })
+});
 // Matches with "/api/cards"
 router.route("/").get(cardController.findAll);
 router.route("/upload").post(cardController.upload);
@@ -14,9 +35,10 @@ router.use("/user", userRoutes);
 
 // Matches with "/api/cards/:id"
 router
-    .route("/:id")
-    .get(cardController.findById)
-    .put(cardController.update)
-    .delete(cardController.remove);
+  .route("/:id")
+  .post(cardController.upload, upload.single('image'))
+  .get(cardController.findById)
+  .put(cardController.update)
+  .delete(cardController.remove);
 
 module.exports = router;
