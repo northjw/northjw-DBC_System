7ecd0d331f70ebedefedcf872af506b118af0f5f
Vllaadd <vlladdiimiir@gmail.com>
2021-01-22 15:31:51 -0500

    nomatch

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -7,7 +7,6 @@ function NoMatch() {
             <h2>Ooops! Page not found. </h2>
             <h1>404</h1>
             <h3>We can't find the page you're looking for.</h3>
-            <button>Go Back</button>
            </div>
         </>
     );
